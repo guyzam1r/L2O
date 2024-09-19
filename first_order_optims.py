ea20a76d49a71cfbd6ade48641236d20d79b27b5
Guy Zamir <55858722+guyzam1r@users.noreply.github.com>
2024-09-18 21:38:04 -0700

    Add files via upload

--- first_order_optims.py
+++ first_order_optims.py
@@ -25,13 +25,15 @@ class Momentum_Opt:
     	self.momentum = 0
 
 class Adam_Opt:
-    def __init__(self, lr=1e-2, mmtm_decay=0.9, sqgd_decay=0.999):
+    def __init__(self, lr=1e-2, mmtm_decay=0.9, sqgd_decay=0.999, teleport=False, theta=0):
         self.lr = lr
         self.mmtm_decay = mmtm_decay
         self.sqgd_decay = sqgd_decay
         self.mmtm = 0
         self.sqgd = 0
         self.k = 1
+        self.teleport=teleport
+        self.theta=theta
         self.is_meta_opt = False
 
     def step(self, grad):
@@ -42,7 +44,29 @@ class Adam_Opt:
     	self.k += 1
     	return -self.lr * corrected_mmtm / (1e-8 + torch.sqrt(corrected_sqgd))
 
+    def theta_step(self, theta_grad=0.0):
+        pass
+
     def reset_params(self):
     	self.momentum = 0
     	self.sqgd = 0
     	self.k = 0
+
+class L2TOTF_Opt:
+    def __init__(self, lr=0.1, theta_lr=0.01, theta_init=0.0):
+        self.lr = lr
+        self.theta_lr = theta_lr
+        self.theta = torch.tensor(theta_init, requires_grad=True)
+        self.teleport = True
+        self.is_meta_opt = False
+
+    def step(self, grad):
+        return -self.lr * grad
+
+    def theta_step(self, theta_grad=0.0):        
+        #update theta
+        if theta_grad * self.theta <= 0:
+            self.theta = self.theta - self.theta_lr * theta_grad
+        else:
+            print("Changing direction!")
+            self.theta = 0*self.theta - 5 * self.theta_lr * theta_grad
